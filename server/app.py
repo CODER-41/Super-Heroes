6c2b13a68123ebd140605c71db5f711063b1273c
RONNY <mboyaronny41@gmail.com>
2025-12-25 12:07:40 +0300

    app.py

--- server/app.py
+++ server/app.py
@@ -1,104 +1,34 @@
-from flask import Flask, request, jsonify, make_response
-
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-from models import db, Hero, Power, HeroPower
+from models import db, Hero, POwer, HeroPower
 import os
-
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = OS.ENVIRON.GET(
-    'DATABASE_URI,
-    'sqlite:///superheroes.db'
-)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.json.compact = False
 
 db.init_app(app)
-
 migrate = Migrate(app, db)
 
-
 @app.route('/')
-def index ():
+def index():
+
     return jsonify({
-        "message": "Superheroes API",
+        "message": "SuperHeroes API",
         "endpoints": {
-            "heroes": "/heroes",           
-            "hero_by_id": "/heroes/:id",      
-            "powers": "/powers",             
-            "power_by_id": "/powers/:id",   
-            "hero_powers": "/hero_powers"     
+            "GET /heroes": "Get all heroes",
+            "GET /heroes/:id": "Get a specific hero",
+            "GET /powers": "Get all powers",
+            "GET /powers/:id": "Get a specific power",
+            "PATCH /powers/:id": "Update a power",
+            "POST /hero_powers": "Create a hero-power association"
         }
     })
 
-
-
 @app.route('/heroes', methods=['GET'])
 def get_heroes():
-    heroes = Hero.query.all()
-
-    heroes_list = [
-        hero.to_dict(only=('id', 'name', 'super_name')) for hero in heroes
-        for hero in heroes
-    ]
-
-    return jsonify(heroes_list), 200
-
-
-@app.route('/heroes/<int:id>', methods=['GET'])
-def get_hero_by_id(id):
-
-    hero = Hero.query.filter_by(id=id).first()
-
-    if not hero:
-        return jsonify({"error": "Hero not found"}), 404
     
 
-    hero_dict = hero.to_dict(only=(
-        'id',
-        'name',
-        'super_name',
-        'hero_powers.id',
-        'hero_powers.hero_id',
-        'hero_powers.power_id',
-        'hero_powers.strength',
-        'hero_powers.power.id',
-        'hero_powers.power.name',
-        'hero_powers.power.description'
-        
 
-    ))
-
-    return jsonify(hero_dict), 200
-
-
-@app.route('/powers', methods=['GET'])
-def get_powers():
-
-    powers = Power.query.all()
-
-    powers_list = [
-        power.to_dict(only=('id', 'name', 'description'))
-         for power in powers
-    ]
-
-    return jsonify(powers_list), 200
-
-
-@app.route('/powers/<int:id>', methods=['GET'])
-def get_power_by_id(id):
-
-    power = Power.query.filter_by(id=id).first()
-
-    if not power:
-        return jsonify({"error": "Power not found"}), 404
-    
-
-    power_dict = power.to_dict(only=('id', 'name', 'description'))
-
-    return jsonify(power_dict), 200
-
-
-    
