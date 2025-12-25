409289f3adc9e0955d84c2f37af5ba5d9730c093
RONNY <mboyaronny41@gmail.com>
2025-12-25 20:16:33 +0300

    seed.py

---
+++ server/seed.py
@@ -0,0 +1,27 @@
+from app import app
+from models import db, Hero, Power, HeroPower
+from random import choice 
+
+def seed_database():
+
+    with app.app_context():
+        print("Clearing existing data...")
+        HeroPower.query.delete()
+        Power.query.delete()
+
+        db.session.commit()
+
+        print("Creating heroes...")
+
+        heroes = [
+            Hero(name="Kamala Khan", super_name="Ms. Marvel"),
+            Hero(name="Doreen Green", super_name="Squirrel Girl"),
+            Hero(name="Gwen Stacy", super_name="Spider-Gwen"),
+            Hero(name="Janet Van Dyne", super_name="The Wasp"),
+            Hero(name="Wanda Maximoff", super_name="Scarlet Witch"),
+            Hero(name="Carol Danvers", super_name="Captain Marvel"),
+            Hero(name="Jean Grey", super_name="Dark Phoenix"),
+            Hero(name="Ororo Munroe", super_name="Storm"),
+            Hero(name="Kitty Pryde", super_name="Shadowcat"),
+            Hero(name="Elektra Natchios", super_name="Elektra")
+        ]
